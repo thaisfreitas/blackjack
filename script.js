896d4fb0df6454c46549189829381a0fdb60ddaf
Thais Moura <tfreitas@thoughtworks.com>
2018-07-01 13:42:37 +0530

    feat: add first var for the blackjack game

---
+++ script.js
@@ -0,0 +1,14 @@
+// 
+//  Blackjack Game
+//  By Thais Freitas with JavaScrit
+//
+
+let card1 = "Ace of Spades", 
+    card2 = "Tem of Herts";
+
+console.log("welcome to Blackjack! ");
+
+console.log("you are dealt: ");
+console.log("  " + card1);
+console.log("  " + card2);
+
