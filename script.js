6f2c6bfb3382ec4331dc8e7f3bc725731148edbc
Thais Moura <tfreitas@thoughtworks.com>
2018-11-09 16:04:59 -0300

    feature: add user interface

--- script.js
+++ script.js
@@ -9,13 +9,49 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
     'Ten', 'Nine', 'Eight', 'Seven', 'Six',
     'Five', 'Four', 'Three', 'Two'];
 
+//DOM Variables    
+let textArea = document.getElementById('text-area'),
+    newGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('hit-button'),
+    stayButton = document.getElementById('stay-button');
+
+//Games Variables    
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = []; 
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+showStatus();
+
+newGameButton.addEventListener('click', function() {
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck= createDeck();
+    dealerCards = [ getNextCard(), getNextCard() ];
+    playerCards = [ getNextCard(), getNextCard() ];
+    
+
+    newGameButton.style.display = 'none'
+    hitButton.style.display = 'inline';
+    stayButton.style.display = 'inline';
+    showStatus();
+});
+
 function createDeck() {
     let deck = [];
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
             let card = {
                 suit: suits[suitIdx],
-                values[valueIdx]
+                values: values[valueIdx]
             };
             deck.push(card);
         }
@@ -31,16 +67,9 @@ function getNextCard() {
     return deck.shift();
 }
 
-let deck = createDeck();
-
-for (let i = 0; i < deck.length; i++) {
-    // console.log(deck[i]);
+function showStatus() {
+    if(!gameStarted){
+        textArea.innerText = 'Welcome to Blackjack';
+        return;
+    }
 }
-
-let playerCards = [getNextCard(), getNextCard()];
-
-console.log(" ----- ----- welcome to Blackjack! ----- -----  ");
-
-console.log("you are dealt: ");
-console.log("  " + getCardString(playerCards[0]));
-console.log("  " + getCardString(playerCards[1]));
