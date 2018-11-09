8268f004c0f60d815827223c47aefd59277b87ab
Thais Moura <tfreitas@thoughtworks.com>
2018-11-09 14:28:34 -0300

    refactor: add objects and functions to blackjacj game

--- script.js
+++ script.js
@@ -4,27 +4,43 @@
 //
 
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+
 let values = ['Ace', 'King', 'Queen', 'Jack',
-    'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
     'Five', 'Four', 'Three', 'Two'];
 
-let deck = [];
-
-for (let suitIdx = 0; suitIdx < suits.length; suitIdx++){
-    for(let valueIdx = 0; valueIdx < values.length; valueIdx++){
-        deck.push(values[valueIdx] + ' of ' + suits[suitIdx] );
+function createDeck() {
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            let card = {
+                suit: suits[suitIdx],
+                values[valueIdx]
+            };
+            deck.push(card);
+        }
     }
+    return deck;
 }
 
-for (let i=0; i<deck.length; i++){
-    console.log(deck[i]);
+function getCardString(card) {
+    return card.value + ' of ' + card.suit;
 }
 
+function getNextCard() {
+    return deck.shift();
+}
 
-let playerCards = [ deck[0], deck[2]];
+let deck = createDeck();
+
+for (let i = 0; i < deck.length; i++) {
+    // console.log(deck[i]);
+}
+
+let playerCards = [getNextCard(), getNextCard()];
 
 console.log(" ----- ----- welcome to Blackjack! ----- -----  ");
 
 console.log("you are dealt: ");
-console.log("  " + playerCards[0]);
-console.log("  " + playerCards[1]);
+console.log("  " + getCardString(playerCards[0]));
+console.log("  " + getCardString(playerCards[1]));
